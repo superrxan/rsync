8e1fcb6a6fa5788292e99ed5187da50f63896654
Jianxiang Ran <rxan_embedded@163.com>
2024-02-07 10:31:51 +0800

    export rsync client

---
+++ rsync_client/sync.go
@@ -0,0 +1,24 @@
+package rsync_client
+
+import (
+	"github.com/gokrazy/rsync/internal/receivermaincmd"
+)
+
+type NopIO struct {
+}
+
+func (nop NopIO) Read(p []byte) (n int, err error) {
+	return len(p), nil
+}
+
+func (nop NopIO) Write(p []byte) (n int, err error) {
+	return len(p), nil
+}
+
+func Sync(address, destination string) error {
+	IO := &NopIO{}
+	args := []string{"gokr-rsync", "-v", "--archive"}
+	args = append(args, address, destination)
+	_, err := receivermaincmd.Main(args, IO, IO, IO)
+	return err
+}
